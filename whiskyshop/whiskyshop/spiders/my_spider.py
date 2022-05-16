6b7bae99604e298c81b364b93764c72c6731f8c9
camus <free7hem@gmail.com>
2022-05-16 12:22:32 +0100

    demo spider

---
+++ whiskyshop/whiskyshop/spiders/my_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+
+
+class MySpiderSpider(scrapy.Spider):
+    name = 'my-spider'
+    allowed_domains = ['www.whiskyshop.com']
+    start_urls = ['https://www.whiskyshop.com/single-malt-scotch-whisky']
+
+    def parse(self, response):
+        products = response.css('div.product-item-info')
+        
+        for product in products:
+            try: 
+                img = product.css('img.product-image-photo').attrib['src']
+            except:
+                img = ''
+            
+            price = product.css('span.price::text').get()
+            price = price.replace('£','') if price is not None else 0.0
+            yield {
+                'name': product.css('a.product-item-link::text').get(),
+                'price': price,
+                'link': product.css('a.product-item-link').attrib['href'],
+                'img': img
+            }
+        
+        next_page = response.css('a.next.action').attrib['href']
+        if next_page is not None:
+            yield response.follow(next_page, callback = self.parse)
